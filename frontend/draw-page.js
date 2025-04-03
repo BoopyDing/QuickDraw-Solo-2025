031bb62cdab008c62e6c941a552cef315f8bf9bf
BoopyDing <simonbeuerman@gmail.com>
2025-04-03 17:23:44 -0400

    added slider, added eraser, added reset button

--- frontend/draw-page.js
+++ frontend/draw-page.js
@@ -1,37 +1,70 @@
 console.log("JS loaded");
 let canvas = document.getElementById("myCanvas");
+let slider = document.getElementById("myRange");
 let ctx = canvas.getContext("2d");
 let isDrawing = false;
+
+ctx.imageSmoothingEnabled = false;
+ctx.webkitImageSmoothingEnabled = false;
+ctx.mozImageSmoothingEnabled = false;
+ctx.msImageSmoothingEnabled = false;
+
+let undoArray = [];
+let redoArray = [];
 
 let lastX;
 let LastY;
 
-ctx.strokeStyle = "black";
+let writingSize = 2;
+
+let writingColor = "black";
+
+ctx.strokeStyle = writingColor;
 ctx.lineWidth = 2;
 ctx.lineCap = "round";
 
+function resetCanvas() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+function pencil() {
+  writingColor = "black";
+}
+
+function eraser() {
+  writingColor = "white";
+}
+
 canvas.addEventListener("mousedown", function (event) {
-  console.log("mouse down");
   lastX = event.offsetX;
   lastY = event.offsetY;
   isDrawing = true;
+  redoArray = [];
+  undoArray = [];
 });
 canvas.addEventListener("mouseup", function (event) {
-  console.log("mouse up");
   isDrawing = false;
 });
 canvas.addEventListener("mousemove", function (event) {
   if (isDrawing) {
-    console.log("drawing");
-    let x = event.offsetX;
-    let y = event.offsetY;
+    let x = Math.round(event.offsetX);
+    let y = Math.round(event.offsetY);
+
+    ctx.strokeStyle = writingColor;
+    ctx.lineWidth = writingSize;
 
     ctx.beginPath();
     ctx.moveTo(lastX, lastY);
     ctx.lineTo(x, y);
     ctx.stroke();
 
+    undoArray.push([lastX, lastY, x, y]);
+
     lastX = x;
     lastY = y;
   }
+});
+
+slider.addEventListener("input", function () {
+  writingSize = slider.value;
 });
